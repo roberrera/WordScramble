f56e587e8b04fc2699e35313523fd1b38593adbc
roberrera <roberterrera@gmail.com>
2016-09-09 15:18:39 -0400

    Allow user to tap a button to open a dialog and submit a word; Check if word is valid and add to list if it is

--- WordScramble/MasterViewController.swift
+++ WordScramble/MasterViewController.swift
@@ -18,6 +18,8 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
             if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
                 allwords = startWords.componentsSeparatedByString("\n")
@@ -32,6 +34,67 @@ class MasterViewController: UITableViewController {
         title = allwords[0]
         objects.removeAll(keepCapacity: true)
         tableView.reloadData()
+    }
+    
+    func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .Alert)
+        ac.addTextFieldWithConfigurationHandler(nil)
+
+        let submitAction = UIAlertAction(title: "Submit", style: .Default) { [unowned self, ac] _ in
+            let answer = ac.textFields![0]
+            self.submitAnswer(answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func submitAnswer(answer: String) {
+        // Receive user input as lowercase.
+        let lowerAnswer = answer.lowercaseString
+        
+        if wordIsPossible(lowerAnswer) {
+            if wordIsOriginal(lowerAnswer) {
+                if wordIsReal(lowerAnswer) {
+                    objects.insert(answer, atIndex: 0)
+                    
+                    // Insert a new row for better visual tracking by the user.
+                    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+                    tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+                }
+            }
+        }
+    }
+    
+    func wordIsPossible(word: String) -> Bool {
+        var tempWord = title!.lowercaseString
+        
+        /* For each character in the user-entered string, remove each character from the original word
+         so that as the loop checks for each character it doesn't find the character twice when
+         it should not. */
+        for letter in word.characters {
+            if let pos = tempWord.rangeOfString(String(letter)) {
+                tempWord.removeAtIndex(pos.startIndex)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func wordIsOriginal(word: String) -> Bool {
+        // Check if the objects list contains the string, and returns true if the string has not been used.
+        return !objects.contains(word)
+    }
+    
+    func wordIsReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSMakeRange(0, word.characters.count)
+        let misspelledRange = checker.rangeOfMisspelledWordInString(word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        // Check if the user input is a real word in the english dictionary, and return true if it is. Return false if it is not.
+        return misspelledRange.location == NSNotFound
     }
 
     override func didReceiveMemoryWarning() {
